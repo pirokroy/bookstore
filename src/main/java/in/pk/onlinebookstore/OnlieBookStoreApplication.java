9daef239bb92fd059f3537e34152001f05e9c960
roy <roy@DESKTOP-3CE11Q9>
2021-01-01 19:45:54 +0530

    Initial Changes2

---
+++ src/main/java/in/pk/onlinebookstore/OnlieBookStoreApplication.java
@@ -0,0 +1,13 @@
+package in.pk.onlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlieBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlieBookStoreApplication.class, args);
+	}
+
+}
